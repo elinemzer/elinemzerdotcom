4ea2bb130fe1680abaa6c92bf7573e235a606780
Eli Nemzer <elinemzer7@gmail.com>
2017-07-28 18:01:35 -0400

    ready to start on travel

--- browser/components/travel.js
+++ browser/components/travel.js
@@ -6,27 +6,23 @@ export default function Travel () {
   return (
     <div id="travel-page">
     <Navbar />
-      <img src="files/img/salvador.jpg" />
-
+      <img src="files/img/fuego.jpg" />
       <div id="travel-text">
       ¿adónde va?
       </div>
 
+      <div className="region">
+      <ul className="nav nav-tabs">
+        <li role="presentation"><a href='#/reading'>usa.</a></li>
+        <li role="presentation"><a href='#/reading'>central america.</a></li>
+        <li role="presentation"><a href='#/reading'>south america.</a></li>
+        <li role="presentation"><a href='#/reading'>europe.</a></li>
 
-      <div className="dropdown">
-        <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown button
-        </button>
-      <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-        <a className="dropdown-item" href="#">Action</a>
-        <a className="dropdown-item" href="#">Another action</a>
-        <a className="dropdown-item" href="#">Something else here</a>
-      </div>
+      </ul>
     </div>
 
-
       <div id="travel-caption">
-      <h5>morro de pelourinho, salvador, bahia</h5>
+      <h5>el hato, sacatepéquez department, guatemala</h5>
       </div>
     </div>
   )
